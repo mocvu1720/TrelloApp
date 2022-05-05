ca04fbc127741155101f65b37797354f21b03895
mocvu1720 <nguyenhuuhuy170820@gmail.com>
2022-05-05 14:12:04 +0700

    Clean architecture

---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Task.scss'
+
+function Task() {
+    return (
+        <li className="task-item">
+            <img src="https://www.toponseek.com/blogs/wp-content/uploads/2019/06/toi-uu-hinh-anh-optimize-image-4-1200x700.jpg" alt="anh" />
+            Title: React
+        </li>
+    )
+}
+
+export default Task
