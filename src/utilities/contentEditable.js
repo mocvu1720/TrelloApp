960f405f1063c56d32d0bcd28e4d4307d00c0c84
mocvu1720 <nguyenhuuhuy170820@gmail.com>
2022-05-11 21:16:17 +0700

    Update and Remove Column

---
+++ src/utilities/contentEditable.js
@@ -0,0 +1,16 @@
+
+// Onkeydown
+
+export const saveContentAfterPressEnter = (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault()
+    e.target.blur()
+  }
+}
+
+// Select all input value when click
+
+export const selectAllInlineText = (e) => {
+  e.target.focus()
+  e.target.select()
+}
